d8c94b2a803bb97b0a2c8fc9f9f306e85a3b5b69
HugoBaptista13 <a23279@alunos.ipca.pt>
2024-10-11 10:50:27 +0100

    Atualizar codigo

--- Calculadora/app/src/main/java/com/hugo/calculadora/MainActivity.kt
+++ Calculadora/app/src/main/java/com/hugo/calculadora/MainActivity.kt
@@ -5,17 +5,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hugo.calculadora.ui.theme.CalculadoraTheme
@@ -27,7 +37,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CalculadoraTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Calculadora(
+                    Calc(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -37,24 +47,127 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@Composable
+fun Calculate() {
+    val number = remember { mutableFloatStateOf(0f) }
+    val operator = remember { mutableStateOf<Char>(' ') }
+    val result = remember { mutableFloatStateOf(0f) }
+
+    when (operator.value) {
+        '+' -> result.value = number.value + number.value
+        '-' -> result.value = number.value - number.value
+        '*' -> result.value = number.value * number.value
+        '/' -> result.value = number.value / number.value
+    }
+}
+
+fun updateNumber(number: String, newNumber: Char): String {
+    if (number == "0" && newNumber == '0') return number
+    if (number.contains('.') && newNumber == '.') return number
+    if (newNumber == 'E'){
+        if (number.length == 1) return "0"
+        return number.dropLast(1)
+    }
+    if (newNumber == 'C') return "0"
+    if (number == "0" && newNumber != '.') return newNumber.toString()
+    return number + newNumber
+}
+
+@Composable
+fun Calc(modifier: Modifier = Modifier) {
+    var number = remember { mutableStateOf("0") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(105, 105, 105))
+            .padding(20.dp)
+    ) {
+        // Display
+        Box(
+            modifier = Modifier
+                .weight(0.4f)
+                .fillMaxWidth()
+                .background(Color(51, 153, 102))
+                .padding(12.dp, 40.dp)
+        ) {
+            Text(
+                text = number.value,
+                textAlign = TextAlign.Right,
+                color = Color.White,
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+
+        // Buttons
+        val buttonModifier = Modifier
+            .weight(0.5f)
+            .fillMaxWidth()
+            .padding(4.dp)
+            .clickable {  }
+
+        val operatorBtnColors = ButtonDefaults.buttonColors((Color(10, 10, 10)))
+        val clearBtnColors = ButtonDefaults.buttonColors((Color(250, 0, 70)))
+        val numberBtnColors = ButtonDefaults.buttonColors((Color(150, 150, 150)))
+
+        Column(modifier = Modifier.weight(1f)) {
+            Row(modifier = Modifier.weight(0.5f)) {
+                Button(onClick = {}, modifier = buttonModifier, colors = operatorBtnColors) { Text("MRC") }
+                Button(onClick = {}, modifier = buttonModifier, colors = operatorBtnColors) { Text("M-") }
+                Button(onClick = {}, modifier = buttonModifier, colors = operatorBtnColors) { Text("M+") }
+                Button(onClick = {number.value = updateNumber(number.value,'C')}, modifier = buttonModifier, colors = clearBtnColors) { Text("ON/C") }
+            }
+            Row(modifier = Modifier.weight(1f)) {
+                Button(onClick = {}, modifier = buttonModifier, colors = operatorBtnColors) { Text("√") }
+                Button(onClick = {}, modifier = buttonModifier, colors = operatorBtnColors) { Text("%") }
+                Button(onClick = {}, modifier = buttonModifier, colors = operatorBtnColors) { Text("+/-") }
+                Button(onClick = {number.value = updateNumber(number.value,'E')}, modifier = buttonModifier, colors = clearBtnColors) { Text("CE") }
+            }
+            Row(modifier = Modifier.weight(0.5f)) {
+                Button(onClick = {number.value = updateNumber(number.value,'7')}, modifier = buttonModifier, colors = numberBtnColors) { Text("7") }
+                Button(onClick = {number.value = updateNumber(number.value,'8')}, modifier = buttonModifier, colors = numberBtnColors) { Text("8") }
+                Button(onClick = {number.value = updateNumber(number.value,'9')}, modifier = buttonModifier, colors = numberBtnColors) { Text("9") }
+                Button(onClick = {}, modifier = buttonModifier, colors = operatorBtnColors) { Text("/") }
+            }
+            Row(modifier = Modifier.weight(0.5f)) {
+                Button(onClick = {number.value = updateNumber(number.value,'4')}, modifier = buttonModifier, colors = numberBtnColors) { Text("4") }
+                Button(onClick = {number.value = updateNumber(number.value,'5')}, modifier = buttonModifier, colors = numberBtnColors) { Text("5") }
+                Button(onClick = {number.value = updateNumber(number.value,'6')}, modifier = buttonModifier, colors = numberBtnColors) { Text("6") }
+                Button(onClick = {}, modifier = buttonModifier, colors = operatorBtnColors) { Text("*") }
+            }
+            Row(modifier = Modifier.weight(0.5f)) {
+                Button(onClick = {number.value = updateNumber(number.value,'1')}, modifier = buttonModifier, colors = numberBtnColors) { Text("1") }
+                Button(onClick = {number.value = updateNumber(number.value,'2')}, modifier = buttonModifier, colors = numberBtnColors) { Text("2") }
+                Button(onClick = {number.value = updateNumber(number.value,'3')}, modifier = buttonModifier, colors = numberBtnColors) { Text("3") }
+                Button(onClick = {}, modifier = buttonModifier, colors = operatorBtnColors) { Text("-") }
+            }
+            Row(modifier = Modifier.weight(0.5f)) {
+                Button(onClick = {number.value = updateNumber(number.value,'0')}, modifier = buttonModifier, colors = numberBtnColors) { Text("0") }
+                Button(onClick = {number.value = updateNumber(number.value,'.')}, modifier = buttonModifier, colors = numberBtnColors) { Text(".") }
+                Button(onClick = {}, modifier = buttonModifier, colors = numberBtnColors) { Text("=") }
+                Button(onClick = {}, modifier = buttonModifier, colors = operatorBtnColors) { Text("+") }
+            }
+        }
+    }
+}
 
 @Composable
 fun Calculadora(modifier: Modifier = Modifier) {
     Box(
         Modifier
             .fillMaxSize()
+            .background(Color(105,105,105))
     )
     {
-        Column(
+        Row(
             Modifier
                 .padding(50.dp)
-                .background(Color(105,105,105))
         )
         {
             //MOSTRADOR
-            Row(
+            Column(
                 Modifier
-                    .padding(20.dp)
+                    .padding(25.dp)
                     .background(Color(51, 153, 102))
                     .fillMaxWidth()
             )
@@ -62,13 +175,16 @@ fun Calculadora(modifier: Modifier = Modifier) {
                 Text("NUMEROS PLACEHOLDER")
             }
         }
-        Column(
+        Row(
             Modifier
-                .padding(30.dp)
+                .padding(40.dp)
         )
         {
             //Linha 1
-            Row()
+            Row(
+                Modifier
+                    .padding(5.dp)
+            )
             {
                 //MRC
                 Column()
@@ -217,8 +333,8 @@ fun Calculadora(modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CalculadoraPreview() {
     CalculadoraTheme {
-        Calculadora()
+        Calc()
     }
 }
